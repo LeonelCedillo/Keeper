9b63c39b31cf053782dc8d00298c32cbe72977ce
Leo <leonel.cs.979@gmail.com>
2025-07-31 22:01:11 -0700

    fix CreateArea.jsx

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          placeholder="Title"
+          value={note.title}
+          onChange={handleChange}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+          onChange={handleChange}
+        />
+        <button
+          onClick={(event) => {
+            props.onAdd(event, note);
+            setNote({
+              title: "",
+              content: "",
+            });
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
